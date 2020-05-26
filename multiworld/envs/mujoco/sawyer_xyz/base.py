79d7443b578a2f7e27898c6c4bd2fbb5d58288dd
kshitijsachan <kshitijsachan39@gmail.com>
2020-05-26 15:29:51 -0500

    breakpoints

--- multiworld/envs/mujoco/sawyer_xyz/base.py
+++ multiworld/envs/mujoco/sawyer_xyz/base.py
@@ -92,6 +92,7 @@ class SawyerXYZEnv(SawyerMocapBase, metaclass=abc.ABCMeta):
             self.mocap_low,
             self.mocap_high,
         )
+        print(pos_delta, new_mocap_pos - self.data.mocap_pos)
         ipdb.set_trace()
         self.data.set_mocap_pos('mocap', new_mocap_pos)
         self.data.set_mocap_quat('mocap', np.array([1, 0, 1, 0]))
